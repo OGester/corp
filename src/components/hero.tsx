c7697fa08239d31be37f64e96dac27ae63306b95
Oskar Gester <gester.oskar@gmail.com>
2025-07-23 19:54:26 +0200

    changed gradient once more to a darker color

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -17,7 +17,7 @@ export default function Hero(props: HeroProps) {
           fill
           style={{ objectFit: "cover" }}
         />
-        <div className="absolute inset-0 bg-gradient-to-r from-[#202023]" />
+        <div className="absolute inset-0 bg-gradient-to-r from-[#101012]" />
       </div>
       <div className="pt-50 md:pt-40 2md:pt-75 flex justify-center items-center">
         <h1 className="font-gothic text-[#f5f6fa] px-1.5 text-3xl md:text-4xl 2md:text-6xl xl:text-7xl text-center">
